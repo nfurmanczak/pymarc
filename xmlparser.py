a806cbe6b06691787595284a1a7f894430180322
root <root@furmanczak.de>
2019-09-05 22:00:18 +0200

    first commit

---
+++ xmlparser.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+
+import xml.etree.ElementTree as ET
+import time
+import mysql.connector
+
+# Connection to SQL Database 
+
+db = mysql.connector.connect(
+    host='localhost',
+    database='dmarc',
+    user='dmarc',
+    password='dmU75sd109Olki'
+)
+
+
+tree = ET.parse('google.com!furmanczak.de!1541376000!1541462399.xml')
+root = tree.getroot()
+
+# Metadaten im REPORT
+print("METADATA: ")
+print("#################")
+for orgname in root.iter('org_name'):
+    print("Quelle: " + orgname.text)
+for reportid in root.iter('report_id'):
+    print("Report ID: " + reportid.text)
+for daterange in root.iter('date_range'):
+    for begin in daterange.iter('begin'):
+        beginDate = time.strftime("%Y-%m-%d", time.localtime(int(begin.text)))
+        print(beginDate)
+    for end in daterange.iter('end'):
+        endDate = time.strftime("%Y-%m-%d", time.localtime(int(end.text)))
+        print(endDate)
+
+
+
+# IP Daten
+print("REPORT:")
+print("#################")
+for record in root.findall('record'):
+    for row in record.findall('row'):
+        for ip in row.findall('source_ip'):
+            print("IP: " + ip.text)
+        for count in row.findall('count'):
+            print("Count: " + count.text)
+        for policy_evaluated in row.findall('policy_evaluated'):
+            for dkim in policy_evaluated.findall('dkim'):
+                print("DKIM: " + dkim.text)
+            for spf in policy_evaluated.findall('spf'):
+                print("SPF:" + spf.text)
+
+reportInsert = "insert into post(title, content, category_id) value(%s, %s, %s, %s)"
+ 
+reportData = [
+    ('title 2', 'content 2', datetime.now.date(), 1),
+    ('title 3', 'content 3', datetime.now.date(), 1),
+    ('title 4', 'content 4', datetime.now.date(), 1),
+    ('title 5', 'content 5', datetime.now.date(), 1),
+    ('title 6', 'content 6', datetime.now.date(), 1),
+]
+ 
+cursor.executemany(sql2, data2)
+ 
+db.commit()  # commit the changes
