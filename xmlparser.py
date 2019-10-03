7bbde905b93b0a0b5373a1853442e0fde8b1c0f7
Nikolai Furmanczak <nikolai@furmanczak.de>
2019-10-03 12:18:59 +0200

    fixed beginDate and endDate

--- xmlparser.py
+++ xmlparser.py
@@ -26,12 +26,12 @@ for reportid in root.iter('report_id'):
     print("Report ID: " + reportid.text)
 for daterange in root.iter('date_range'):
     for begin in daterange.iter('begin'):
-        beginDate = time.strftime("%Y-%m-%d", time.localtime(int(begin.text)))
+        # MySQL DateTime format: YYYY-MM-DD HH:MM:SS
+        beginDate = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(int(begin.text)))
         print(beginDate)
     for end in daterange.iter('end'):
-        endDate = time.strftime("%Y-%m-%d", time.localtime(int(end.text)))
+        endDate = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(int(end.text)))
         print(endDate)
-
 
 
 # IP Daten
@@ -48,17 +48,18 @@ for record in root.findall('record'):
                 print("DKIM: " + dkim.text)
             for spf in policy_evaluated.findall('spf'):
                 print("SPF:" + spf.text)
+        print("IPX") 
 
-reportInsert = "insert into post(title, content, category_id) value(%s, %s, %s, %s)"
+#reportInsert = "insert into reports(source, reportid, startdate, enddate, ip, msgcount, spf, dkim) value(%s, %s, %s, %s, %s, %s, %s, %s)"
  
-reportData = [
-    ('title 2', 'content 2', datetime.now.date(), 1),
-    ('title 3', 'content 3', datetime.now.date(), 1),
-    ('title 4', 'content 4', datetime.now.date(), 1),
-    ('title 5', 'content 5', datetime.now.date(), 1),
-    ('title 6', 'content 6', datetime.now.date(), 1),
-]
+#reportData = [
+#    ('title 2', 'content 2', datetime.now.date(), 1),
+#    ('title 3', 'content 3', datetime.now.date(), 1),
+#    ('title 4', 'content 4', datetime.now.date(), 1),
+#    ('title 5', 'content 5', datetime.now.date(), 1),
+#    ('title 6', 'content 6', datetime.now.date(), 1),
+#]
  
-cursor.executemany(sql2, data2)
- 
-db.commit()  # commit the changes
+#cursor.executemany(sql2, data2)
+# 
+#db.commit()  # commit the changes
